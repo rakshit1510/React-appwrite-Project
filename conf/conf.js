50589d6c70e90dea3c8c144ab009e91c1535fb49
rakshit1510 <gargrakshit10@gmail.com>
2025-03-15 12:38:40 +0530

    project created

---
+++ conf/conf.js
@@ -0,0 +1,11 @@
+ const conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(impoort.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(impoort.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId : String(impoort.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId : String(impoort.meta.env.VITE_APPWRITE_BUCKET_ID ),
+    
+    
+    
+ }
+ export default conf 
